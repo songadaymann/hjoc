9f6fb057e9241bea33075e206fe08cd6ea34fa5c
songadaymann <37905584+songadaymann@users.noreply.github.com>
2025-05-27 09:53:01 -0400

    fix

--- src/components/Calendar.tsx
+++
@@ -1,9 +0,0 @@
-export default function Calendar() {
-  return (
-    <div className="container mx-auto my-8 p-4 border rounded">
-      <h2 className="text-2xl font-bold mb-4">Upcoming Events</h2>
-      <p>Calendar will go here...</p>
-      {/* Calendar implementation will be added here */}
-    </div>
-  );
-} 
--- src/components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-gray-200 text-center p-4 mt-8">
-      <p>&copy; {new Date().getFullYear()} My Awesome Site. All rights reserved.</p>
-    </footer>
-  );
-} 
